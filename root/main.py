05a0eedf9ab23c866ce25f59ce4238a38cb81c23
sananymore <zhludkop@gmail.com>
2024-05-21 17:37:54 +0200

    Disnake

--- root/main.py
+++ root/main.py
@@ -1,15 +1,19 @@
-import nextcord
-from dotenv import load_dotenv
+import disnake
+from disnake.ext import commands
+import os
 
-load_dotenv()
+bot = commands.Bot(command_prefix='/')
 
-def main():
-    token = open('token').read().strip()
-    intents = nextcord.Intents.default()
-    client = nextcord.Client(intents=intents)
-    client.run(token)
-    print('Bot started')
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
 
-if __name__ == '__main__':
-    main()
+@bot.slash_command(name='ping', description='Ping me!')
+async def ping(ctx):
+    embed = disnake.Embed(title='Pong!', description=f'Bot response time: {round(bot.latency * 1000)}ms')
+    await ctx.send(embed=embed)
 
+with open('token', 'r') as f:
+    token = f.read()
+
+bot.run(token)
